dec95578af7299a974510a10520d413d0e72a567
Justin Lee <justin.r.lee@gmail.com>
2019-07-18 20:41:50 -0400

    Clean up and fix

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM python:3.7-slim
+FROM python:3.7-alpine
 
 RUN pip3 install requests
 
-ADD app.py /app.py
+ADD app.py /
 
-ENTRYPOINT ["python3", "/app.py"]
+# RUN chmod +x
+ENV PYTHONUNBUFFERED=1
+
+ENTRYPOINT ["/usr/local/bin/python3", "/app.py"]
--- app.py
+++ app.py
@@ -20,61 +20,72 @@ import requests
 #   https://api.datadoghq.com/api/v1/series?api_key=XYZ'
 
 if __name__ == '__main__':
+    print("Starting demo datadog app.")
     parser = argparse.ArgumentParser(description='Python script that generates dummy metrics and sends them to Datadog')
 
     parser.add_argument("-i", "--interval",
                         help="Metric reporting interval (in seconds)",
                         type=int,
-                        default=5)
+                        default=os.environ.get('DD_INTERVAL', 5))
 
-    parser.add_argument("-b", "--batch",
+    parser.add_argument("-b", "--batch-size",
                         help="Number of metrics to send at a time",
                         type=int,
-                        default=12)
+                        default=os.environ.get('DD_BATCH_SIZE', 24))
 
     parser.add_argument("-m", "--mean",
                         help="Metric average value",
                         type=int,
-                        default=100)
+                        default=os.environ.get('DD_MEAN', 100))
 
     parser.add_argument("-s", "--standard-dev",
                         help="Metric standard deviation",
                         type=int,
-                        default=20)
+                        default=os.environ.get('DD_STANDARD_DEV', 20))
 
     parser.add_argument("-n", "--metric-name",
-                        help="Metric name")
+                        help="Metric name",
+                        default=os.environ.get('DD_METRIC_NAME', 'custom.metric'))
 
     parser.add_argument("-a", "--app-name",
-                        help="Metric name")
+                        help="App name",
+                        default=os.environ.get('DD_APP_NAME', 'default'))
 
     parser.add_argument("-e", "--env-name",
-                        help="Metric name")
+                        help="Environment name",
+                        default=os.environ.get('DD_ENV_NAME', 'default'))
 
-    parser.add_argument("-k", "--api-key")
+    parser.add_argument("-v", "--version",
+                        default=os.environ.get('DD_VERSION', '1.0'))
+
+    parser.add_argument("-k", "--api-key",
+                        default=os.environ.get('DD_API_KEY', None))
 
     args = parser.parse_args()
 
-    print(args)
+    # print(args)
 
-    interval = args.interval
-    batch    = args.batch
-    mean     = args.mean
-    sd       = args.standard_dev
-    name     = args.metric_name
-    
+    interval     = args.interval
+    batch_size   = args.batch_size
+    mean         = args.mean
+    standard_dev = args.standard_dev
+    metric_name  = args.metric_name
+    app_name     = args.app_name
+    env_name     = args.env_name
+    version      = args.version
+
+    print("Generating metrics every " + str(interval) + " second(s), batching metrics in groups of " + str(batch_size))
+
     tags = []
-    app_name = args.app_name
-    env_name = args.env_name
     
     if app_name is not None:
         tags.append("application:" + app_name)
     if env_name is not None:
         tags.append("environment:" + env_name)
+    if version is not None:
+        tags.append("version:" + version)
 
     key      = args.api_key
-    if key == None:
-        key = os.environ['DD_API_KEY']
     url      = "https://api.datadoghq.com/api/v1/series?api_key=" + key
 
     # print(url)
@@ -82,14 +93,16 @@ if __name__ == '__main__':
     
     metrics = []
     while True:
-        metrics.append([time.time(), random.gauss(mean, sd)])
+        metric = [time.time(), random.gauss(mean, standard_dev)]
+        print(metric)
+        metrics.append(metric)
         time.sleep(interval)
 
-        if (len(metrics) >= batch):
+        if (len(metrics) >= batch_size):
             print(metrics)
             payload = {
                 "series": [{
-                    "metric": name,
+                    "metric": metric_name,
                     "points": metrics,
                     "type": "gauge",
                     "tags": tags
